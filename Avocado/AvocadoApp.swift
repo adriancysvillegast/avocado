c5d75c1cfb0011501e7295aff387726cbfeb2897
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-09-28 15:29:58 -0400

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Adriancys Jesus Villegas Toro on 28/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocado/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocado
+//
+//  Created by Adriancys Jesus Villegas Toro on 28/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
